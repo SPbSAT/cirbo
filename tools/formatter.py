4f67f1d08fc6e464bd4acd4824600f812d6e8b0c
spefk <me@spefk.ru>
2024-06-16 22:52:19 +0300

    fix

---
+++ tools/formatter.py
@@ -0,0 +1,19 @@
+"""
+Module defines utility that formats project's code
+using "black", "docformatter" and "usort".
+"""
+
+from typing import Mapping
+from typing import Container
+import subprocess
+
+
+def _format_project():
+    usort_rc = subprocess.call("poetry run usort format boolean_circuit_tool tests tools", shell=True)
+    docformatter_rc = subprocess.call("poetry run docformatter --in-place boolean_circuit_tool/ tools/", shell=True)
+    black_rc = subprocess.call("poetry run black boolean_circuit_tool/ tests/", shell=True)
+    return any([usort_rc, docformatter_rc, black_rc])
+
+
+if __name__ == '__main__':
+    print(_format_project())
