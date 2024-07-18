4d984fe69d14752db9a05867a0ffa608046c6d14
PinochetLab <you@example.com>
2024-07-18 12:30:10 +0300

    Made class PythonFunction

--- boolean_circuit_tool/core/utils.py
+++ boolean_circuit_tool/core/utils.py
@@ -5,6 +5,7 @@ import typing as tp
 
 __all__ = [
     'input_to_canonical_index',
+    'canonical_index_to_input',
     'get_bit_value',
 ]
 
@@ -23,6 +24,25 @@ def input_to_canonical_index(inputs: tp.Iterable[bool]) -> int:
 
     """
     return int(''.join(str(int(v)) for v in inputs), 2)
+
+
+def canonical_index_to_input(index: int, input_size: int) -> tp.Sequence[bool]:
+    """
+    Get input value sequence by index.
+
+    :param index: index in canonical input enumeration.
+    :param input_size: count of inputs.
+    :return: input value sequence.
+
+    Note: canonical input enumeration is achieved by listing unsigned numbers
+    given in a big-endian binary encoding ([0, ..., 0, 0], [0, ..., 0, 1],
+    [0, ..., 1, 0], [0, ..., 1, 1], ..., [1, ..., 1, 1]), where `i`th bit
+    represents value of `i`th input.
+
+    """
+    s = bin(index)[2:]
+    s = '0' * (input_size - len(s)) + s
+    return [bool(int(c)) for c in s]
 
 
 def get_bit_value(value: int, bit_idx: int) -> bool:
