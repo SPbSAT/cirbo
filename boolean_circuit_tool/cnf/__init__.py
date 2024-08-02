a82b6a25fe6209e146d8f0e65ea2c53170e947dd
Aleksey Vorobiev <78642617+PinochetLab@users.noreply.github.com>
2024-08-03 04:37:17 +0700

    feat sat: tseytin (#26)
    Authored-by: PinochetLab <you@example.com>

---
+++ boolean_circuit_tool/cnf/__init__.py
@@ -0,0 +1,5 @@
+from boolean_circuit_tool.cnf.cnf import Clause, Cnf, CnfRaw, Lit
+from boolean_circuit_tool.cnf.tseytin import tseytin_transformation
+
+
+__all__ = ['Lit', 'Clause', 'CnfRaw', 'Cnf', 'tseytin_transformation']
---
+++ boolean_circuit_tool/cnf/cnf.py
@@ -0,0 +1,50 @@
+import typing as tp
+
+from boolean_circuit_tool.core.circuit import Circuit
+
+
+__all__ = ['Cnf', 'Lit', 'Clause', 'CnfRaw']
+
+
+Lit = int
+Clause = list[Lit]
+CnfRaw = list[Clause]
+
+
+class Cnf:
+    """Class for CNF storage."""
+
+    @staticmethod
+    def from_circuit(circuit: Circuit) -> 'Cnf':
+        """
+        Converts circuit to CNF by Tseytin transformation and returns CNF.
+
+        :param circuit: Circuit what will be converted to cnf.
+
+        """
+        from boolean_circuit_tool.cnf.tseytin import tseytin_transformation
+
+        return tseytin_transformation(circuit)
+
+    def __init__(self, cnf: tp.Optional[CnfRaw] = None):
+        """
+
+        :param cnf: CNF can be not assigned, it means cnf is empty.
+        """
+        if cnf is None:
+            self._cnf = []
+        else:
+            self._cnf = cnf
+
+    def add_clause(self, clause: Clause):
+        """
+        Add clause to CNF.
+
+        :param clause: new clause.
+
+        """
+        self._cnf.append(clause)
+
+    def get_raw(self) -> CnfRaw:
+        """Returns CnfRaw object."""
+        return self._cnf
