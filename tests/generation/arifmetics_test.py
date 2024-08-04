96ffe0eb3af1f057f9a0eebe3e4aa5ae1d5ca4f7
George Levsov <144834227+HSPyroblast@users.noreply.github.com>
2024-08-04 19:24:00 +0300

    Update arifmetics_test.py
    Сorrect version of arifmetics_test.py

--- tests/generation/arifmetics_test.py
+++ tests/generation/arifmetics_test.py
@@ -22,7 +22,7 @@ def to_bin(n, out_len):
     out = []
     for i in range(out_len):
         out.append(n%2)
-        n /= 2
+        n //= 2
     return out[::-1]
 
 def to_num(inputs):
@@ -42,31 +42,56 @@ def mul_naive(inputs_a, inputs_b):
 
     return to_bin(a*b, out_len)
 
-def test_mul():
-    functions = [
+def square_naive(inputs_a):
+    a = to_num(inputs_a)  
+
+    out_len = 2*len(inputs_a)
+    if(len(inputs_a) == 1):
+        out_len -= 1
+
+    return to_bin(a**2, out_len)
+
+@pytest.mark.parametrize("func", [
         add_mul,
         add_mul_alter,
         add_mul_dadda,
         add_mul_wallace,
         add_mul_pow2_m1,
         add_mul_karatsuba
-    ]
-    sizes = [[1, 1], [1, 7], [7, 1], [3, 6], [8, 2], [64, 64]]
-    for funct in functions:
-        for [x, y] in sizes:
-            ckt = Circuit()
-            input_labels = [f'x{i}' for i in range(x+y)]
-            for i in range(n):
-                ckt.add_gate(Gate(input_labels[i], INPUT))
+    ])
+@pytest.mark.parametrize("size", [[1, 1], [1, 7], [7, 1], [3, 6], [8, 2], [16, 16], [24, 15]])
+def test_mul(func, size):
+    x, y = size
+    ckt = Circuit()
+    input_labels = [f'x{i}' for i in range(x+y)]
+    for i in range(x+y):
+        ckt.add_gate(Gate(input_labels[i], INPUT))
 
-            res = funct(ckt, input_labels[:x], input_labels[x:])
-            for i in res:
-                ckt.mark_as_output(i)
+    res = func(ckt, input_labels[:x], input_labels[x:])
+    for i in res:
+        ckt.mark_as_output(i)
 
-            for test in range(1000):
-                input_labels_a = [random.choice([0, 1]) for _ in range(x)]
-                input_labels_b = [random.choice([0, 1]) for _ in range(y)]
-                assert mul_naive(input_labels_a, input_labels_b) == ckt.evaluate(input_labels_a + input_labels_b)
-            
+    for test in range(1000):
+        input_labels_a = [random.choice([0, 1]) for _ in range(x)]
+        input_labels_b = [random.choice([0, 1]) for _ in range(y)]
+        assert mul_naive(input_labels_a, input_labels_b) == ckt.evaluate(input_labels_a + input_labels_b)[::-1]
 
+@pytest.mark.parametrize("func", [
+        add_square, 
+        add_square_pow2_m1
+    ])
+@pytest.mark.parametrize("x", [1, 2, 5, 7, 17, 60])
+def test_square(func, x):   
+    ckt = Circuit()
+    input_labels = [f'x{i}' for i in range(x)]
+    for i in range(x):
+        ckt.add_gate(Gate(input_labels[i], INPUT))
+
+    res = func(ckt, input_labels[:x])
+    for i in res:
+        ckt.mark_as_output(i)
+
+    for test in range(1000):
+        input_labels_a = [random.choice([0, 1]) for _ in range(x)]
+        assert square_naive(input_labels_a) == ckt.evaluate(input_labels_a)[::-1] 
 
