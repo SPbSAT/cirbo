cb3f6c4dd847624285989007d507623cc51487f0
pinochet <a.vorobev1@g.nsu.ru>
2024-08-06 16:04:50 +0700

    made tests

--- boolean_circuit_tool/sat/sat.py
+++ boolean_circuit_tool/sat/sat.py
@@ -12,5 +12,9 @@ def is_satisfiable(circuit: Circuit) -> bool:
     for clause in Cnf.from_circuit(circuit).get_raw():
         g.append(g)
     with pysat.solvers.Lingeling(bootstrap_with=g.clauses, with_proof=True) as l:
-        ...
         return l.solve()
+
+
+def check():
+    for size in range(3, 16):
+
---
+++ tests/boolean_circuit_tool/sat/sat_test.py
@@ -0,0 +1,21 @@
+import pytest
+import typing as tp
+
+from tests.boolean_circuit_tool.cnf.tseytin_test import generate_circuit1, generate_circuit2, generate_circuit3, generate_circuit4
+from boolean_circuit_tool.cnf import CnfRaw, Cnf
+from boolean_circuit_tool.core.circuit import Circuit
+from boolean_circuit_tool.sat import is_satisfiable
+
+
+@pytest.mark.parametrize(
+    'generate_circuit, is_sat',
+    [
+        (generate_circuit1, True),
+        (generate_circuit2, True),
+        (generate_circuit3, True),
+        (generate_circuit4, True),
+    ],
+)
+def test_tseytin(generate_circuit: tp.Callable[[], tp.Tuple[Circuit, CnfRaw]], is_sat: bool):
+    circuit, _ = generate_circuit()
+    assert is_satisfiable(circuit) == is_sat
