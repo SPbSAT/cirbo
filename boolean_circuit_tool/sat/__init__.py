595b06ca51c984f7d43fa6b3aff7a8f128d665ff
pinochet <a.vorobev1@g.nsu.ru>
2024-08-06 15:55:07 +0700

    made is_satisfiable

---
+++ boolean_circuit_tool/sat/__init__.py
@@ -0,0 +1,4 @@
+from boolean_circuit_tool.sat.sat import is_satisfiable
+
+
+__all__ = ['is_satisfiable']
---
+++ boolean_circuit_tool/sat/sat.py
@@ -0,0 +1,16 @@
+import pysat
+
+from boolean_circuit_tool.core.circuit import Circuit
+from boolean_circuit_tool.cnf import Cnf
+
+
+__all__ = ['is_satisfiable']
+
+
+def is_satisfiable(circuit: Circuit) -> bool:
+    g = pysat.formula.CNF()
+    for clause in Cnf.from_circuit(circuit).get_raw():
+        g.append(g)
+    with pysat.solvers.Lingeling(bootstrap_with=g.clauses, with_proof=True) as l:
+        ...
+        return l.solve()
