7b0951e533973a144a10b3ac265ebdc92b7778bf
tbelova <yukikomodo@gmail.com>
2024-08-11 16:11:51 +0300

    resolving PR comments (3): tests

--- tests/boolean_circuit_tool/sat/miter_test.py
+++ tests/boolean_circuit_tool/sat/miter_test.py
@@ -1,38 +1,24 @@
+import pytest
+
 from boolean_circuit_tool.core.circuit import Circuit
+from boolean_circuit_tool.sat import is_circuit_satisfiable
 from boolean_circuit_tool.sat.miter import build_miter
 from boolean_circuit_tool.synthesis.generation import generate_inputs, generate_plus_one
 
 
-def to_list_of_bool(n, bit_len):
-    bit_str = "{0:b}".format(n).zfill(bit_len)
-    res = []
-    bit_str_len = len(bit_str)
-    for i in range(bit_str_len - 1, bit_str_len - bit_len - 1, -1):
-        res.append(bool(int(bit_str[i])))
-    return res
+@pytest.mark.parametrize("n", range(2, 9))
+def test_miter(n: int):
+    plus_zero = generate_inputs(n)
+    plus_one = generate_plus_one(inp_len=n, out_len=n)
+    plus_two = (
+        Circuit()
+        .add_circuit(plus_one, name='first')
+        .extend_circuit(plus_one, name='second')
+    )
+    plus_three = Circuit().add_circuit(plus_two).extend_circuit(plus_one, name='third')
 
-
-def test_miter():
-    for n in range(2, 10):
-        plus_zero = generate_inputs(n)
-        plus_one = generate_plus_one(inp_len=n, out_len=n)
-        plus_two = (
-            Circuit()
-            .add_circuit(plus_one, name='first')
-            .extend_circuit(plus_one, name='second')
-        )
-        plus_three = (
-            Circuit().add_circuit(plus_two).extend_circuit(plus_one, name='third')
-        )
-
-        circuits = [plus_zero, plus_one, plus_two, plus_three]
-        for c1 in circuits:
-            for c2 in circuits:
-                miter = build_miter(c1, c2)
-                sat_miter = False
-                for i in range(2**n):
-                    sat_miter = sat_miter or miter.evaluate(to_list_of_bool(i, n))[0]
-                if c1 == c2:
-                    assert sat_miter == False
-                else:
-                    assert sat_miter == True
+    circuits = [plus_zero, plus_one, plus_two, plus_three]
+    for c1 in circuits:
+        for c2 in circuits:
+            miter = build_miter(c1, c2)
+            assert is_circuit_satisfiable(miter).answer == (c1 != c2)
--- tests/boolean_circuit_tool/synthesis/circuit_generation_test.py
+++ tests/boolean_circuit_tool/synthesis/circuit_generation_test.py
@@ -1,3 +1,5 @@
+import pytest
+
 from boolean_circuit_tool.core.utils import canonical_index_to_input
 from boolean_circuit_tool.synthesis.generation import (
     generate_if_then_else,
@@ -16,57 +18,57 @@ def to_list_of_bool(n, bit_len):
     return res
 
 
-def test_generate_plus_one():
-    for n in range(1, 10):
-        for m in range(1, 20):
-            circuit = generate_plus_one(n, m)
-            for i in range(2**n):
-                inp = to_list_of_bool(i, n)
-                out = to_list_of_bool(i + 1, m)
+@pytest.mark.parametrize("n", range(1, 10))
+@pytest.mark.parametrize("m", range(1, 20))
+def test_generate_plus_one(n: int, m: int):
+    circuit = generate_plus_one(n, m)
+    for i in range(2**n):
+        inp = to_list_of_bool(i, n)
+        out = to_list_of_bool(i + 1, m)
 
-                test_out = circuit.evaluate(inp)
-                assert test_out == out
+        test_out = circuit.evaluate(inp)
+        assert test_out == out
 
 
-def test_generate_if_then_else():
+@pytest.mark.parametrize("if_inp", [True, False])
+@pytest.mark.parametrize("then_inp", [True, False])
+@pytest.mark.parametrize("else_inp", [True, False])
+def test_generate_if_then_else(if_inp: bool, then_inp: bool, else_inp: bool):
     circuit = generate_if_then_else()
-    for if_inp in [True, False]:
-        for then_inp in [True, False]:
-            for else_inp in [True, False]:
-                if if_inp:
-                    real_res = then_inp
-                else:
-                    real_res = else_inp
-                test_res = circuit.evaluate([if_inp, then_inp, else_inp])[0]
-                assert test_res == real_res
+    if if_inp:
+        real_res = then_inp
+    else:
+        real_res = else_inp
+    test_res = circuit.evaluate([if_inp, then_inp, else_inp])[0]
+    assert test_res == real_res
 
 
-def test_generate_pairwise_if_then_else():
-    for n in range(5):
-        all_bit_strings = [canonical_index_to_input(i, n) for i in range(2**n)]
-        circuit = generate_pairwise_if_then_else(n)
-        for if_inp in all_bit_strings:
-            for then_inp in all_bit_strings:
-                for else_inp in all_bit_strings:
-                    real_res = []
-                    for i in range(n):
-                        if if_inp[i]:
-                            real_res.append(then_inp[i])
-                        else:
-                            real_res.append(else_inp[i])
-                    test_out = circuit.evaluate(
-                        list(if_inp) + list(then_inp) + list(else_inp)
-                    )
-                    assert test_out == real_res
-
-
-def test_generate_pairwise_xor():
-    for n in range(5):
-        all_bit_strings = [canonical_index_to_input(i, n) for i in range(2**n)]
-        circuit = generate_pairwise_xor(n)
-
-        for x in all_bit_strings:
-            for y in all_bit_strings:
-                test_out = circuit.evaluate(list(x) + list(y))
-                real_res = [x[i] ^ y[i] for i in range(n)]
+@pytest.mark.parametrize("n", range(1, 5))
+def test_generate_pairwise_if_then_else(n: int):
+    all_bit_strings = [canonical_index_to_input(i, n) for i in range(2**n)]
+    circuit = generate_pairwise_if_then_else(n)
+    for if_inp in all_bit_strings:
+        for then_inp in all_bit_strings:
+            for else_inp in all_bit_strings:
+                real_res = []
+                for i in range(n):
+                    if if_inp[i]:
+                        real_res.append(then_inp[i])
+                    else:
+                        real_res.append(else_inp[i])
+                test_out = circuit.evaluate(
+                    list(if_inp) + list(then_inp) + list(else_inp)
+                )
                 assert test_out == real_res
+
+
+@pytest.mark.parametrize("n", range(1, 5))
+def test_generate_pairwise_xor(n: int):
+    all_bit_strings = [canonical_index_to_input(i, n) for i in range(2**n)]
+    circuit = generate_pairwise_xor(n)
+
+    for x in all_bit_strings:
+        for y in all_bit_strings:
+            test_out = circuit.evaluate(list(x) + list(y))
+            real_res = [x[i] ^ y[i] for i in range(n)]
+            assert test_out == real_res
