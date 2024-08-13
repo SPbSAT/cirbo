367258a602db551bbd3244cfb8f3be1df1910a39
vikria <krivogornitsyna.va@gmail.com>
2024-08-13 22:09:21 +0300

    small fix

--- tutorial/cleaning_majority7_circuit_abc.py
+++ tutorial/cleaning_majority7_circuit_abc.py
@@ -1,9 +1,9 @@
 from boolean_circuit_tool.synthesis.generation.arithmetics.summation import add_sum_n_bits
-from boolean_circuit_tool.synthesis.generation.generation import generate_inputs
+from boolean_circuit_tool.core.circuit import Circuit
 from abc_wrapper import run_abc_commands
 
 
-ckt = generate_inputs(7)
+ckt = Circuit.bare_circuit(7)
 lbls = add_sum_n_bits(ckt, ckt.inputs, basis='AIG')
 ckt.set_outputs([lbls[-1]])
 ckt_after_abc_str = run_abc_commands(ckt.into_bench().format_circuit(), 'strash; dc2')
