6e2b17802d6f17c6834599b4a78852b4ab367bdc
Alexander S. Kulikov <alexander.s.kulikov@gmail.com>
2024-08-13 21:30:46 +0200

    Add ABC cleaning example

---
+++ tutorial/cleaning_majority7_circuit_abc.py
@@ -0,0 +1,10 @@
+from boolean_circuit_tool.synthesis.generation.arithmetics.summation import add_sum_n_bits
+from boolean_circuit_tool.synthesis.generation.generation import generate_inputs
+from abc_wrapper import run_abc_commands
+
+
+ckt = generate_inputs(7)
+lbls = add_sum_n_bits(ckt, ckt.inputs, basis='AIG')
+ckt.set_outputs([lbls[-1]])
+ckt_after_abc_str = run_abc_commands(ckt.into_bench().format_circuit(), 'strash; dc2')
+ckt_after_abc = Circuit.from_bench_string(ckt_after_abc_str)
