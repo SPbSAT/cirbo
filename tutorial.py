ce30098544d8b0dffcadff367bcb463ce055e829
egdegd <egd03072000@gmail.com>
2024-08-13 19:48:29 +0200

    add code for abc running

--- tutorial.py
+++ tutorial.py
@@ -135,3 +135,15 @@ w1, w2 = add_sum_n_bits(ckt, [a1, b1])
 ckt.mark_outputs([b0, w1, w2])
 # minimizing.minimize(ckt)
 minimize_subcircuits(ckt)
+
+
+# ABC code
+
+from boolean_circuit_tool.synthesis.generation.arithmetics.summation import add_sum_n_bits
+from boolean_circuit_tool.synthesis.generation.generation import generate_inputs
+from abc_wrapper import run_abc_commands
+ckt = generate_inputs(7)
+lbls = add_sum_n_bits(ckt, ckt.inputs, basis="AIG")
+ckt.set_outputs([lbls[-1]])
+ckt_after_abc_str = run_abc_commands(ckt.into_bench().format_circuit(), "strash; dc2")
+ckt_after_abc = Circuit.from_bench_string(ckt_after_abc_str)
