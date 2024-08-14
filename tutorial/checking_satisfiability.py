7546afcfb0ff3c03b0563ad88380423c46a0d7cd
Alexander S. Kulikov <alexander.s.kulikov@gmail.com>
2024-08-14 07:49:23 +0300

    Cosmetics

--- tutorial/checking_satisfiability.py
+++ tutorial/checking_satisfiability.py
@@ -2,7 +2,7 @@ from boolean_circuit_tool.core.circuit import Circuit
 from boolean_circuit_tool.sat import is_circuit_satisfiable
 
 
-path_circuit = '../data/circuit.bench'
-ckt = Circuit().from_bench_file(path_circuit)
+path = '../data/circuit.bench'
+ckt = Circuit().from_bench_file(path)
 result = is_circuit_satisfiable(ckt)
 print(result.answer)
--- tutorial/synthesizing_full_adder.py
+++ tutorial/synthesizing_full_adder.py
@@ -7,6 +7,6 @@ def sum_3(x1, x2, x3):
     return [(s >> i) & 1 for i in range(2)]
 
 func = PyFunction.from_positional(sum_3)
-circuit_finder = CircuitFinderSat(func, 5, basis="XAIG")
+circuit_finder = CircuitFinderSat(func, 5, basis='XAIG')
 circuit = circuit_finder.find_circuit()
 circuit.into_graphviz_digraph().view()
