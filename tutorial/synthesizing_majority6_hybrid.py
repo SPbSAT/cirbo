9fec7ac593d09b91962699edc9b95a8b7f320ca0
tbelova <yukikomodo@gmail.com>
2024-08-15 23:56:51 +0300

    cosmetics

--- tutorial/synthesizing_majority6_hybrid.py
+++ tutorial/synthesizing_majority6_hybrid.py
@@ -1,16 +1,18 @@
-from boolean_circuit_tool.core import DontCare, PyFunction
+from boolean_circuit_tool.core import DontCare, PyFunctionModel
 from boolean_circuit_tool.synthesis.circuit_search import CircuitFinderSat
 from boolean_circuit_tool.synthesis.generation.arithmetics import generate_sum_n_bits
 
 
 def geq3_py(x: bool, y: bool, z: bool):
     s = x + 2 * y + 4 * z
-    return [DontCare] if s > 6 else [True] if s >= 3 else [False]
+    return [DontCare] if s > 6 else [True] \
+        if s >= 3 else [False]
 
 
 ckt = generate_sum_n_bits(n=6)
-func = PyFunction.from_positional(geq3_py)
-finder = CircuitFinderSat(func, 2, basis='XAIG')
+func = PyFunctionModel.from_positional(geq3_py)
+finder = CircuitFinderSat(
+    func, 2, basis='XAIG')
 geq3 = finder.find_circuit()
 ckt.extend_circuit(geq3,name='geq3')
 ckt.view_graph()
