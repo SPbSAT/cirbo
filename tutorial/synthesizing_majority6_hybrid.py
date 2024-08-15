c8538e2de406d8be3c156c8f784e0fcdeb887891
tbelova <yukikomodo@gmail.com>
2024-08-16 00:02:48 +0300

    cosmetics

--- tutorial/synthesizing_majority6_hybrid.py
+++ tutorial/synthesizing_majority6_hybrid.py
@@ -5,14 +5,14 @@ from boolean_circuit_tool.synthesis.generation.arithmetics import generate_sum_n
 
 def geq3_py(x: bool, y: bool, z: bool):
     s = x + 2 * y + 4 * z
-    return [DontCare] if s > 6 else [True] \
-        if s >= 3 else [False]
+    return [DontCare] if s > 6 else [True] if s >= 3 else [False]
 
 
 ckt = generate_sum_n_bits(n=6)
-func = PyFunctionModel.from_positional(geq3_py)
+func = PyFunctionModel.from_positional(
+    geq3_py)
 finder = CircuitFinderSat(
     func, 2, basis='XAIG')
 geq3 = finder.find_circuit()
-ckt.extend_circuit(geq3,name='geq3')
+ckt.extend_circuit(geq3, name='geq3')
 ckt.view_graph()
