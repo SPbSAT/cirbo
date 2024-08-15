d45f59f8e81fe24382df3b431716b61c6af13e20
Fedor Kurmazov <me@spefk.ru>
2024-08-15 20:35:28 +0300

    fix tutorial: several micro fixes (#67)

--- tutorial/analyzing_functions.py
+++ tutorial/analyzing_functions.py
@@ -1,4 +1,4 @@
-from boolean_circuit_tool.core import PyFunction
+from boolean_circuit_tool.core import PyFunction, TruthTable
 
 
 f = PyFunction(lambda xs: [sum(xs) % 2], 4)
@@ -7,3 +7,6 @@ print(f.is_monotone())
 g = PyFunction.from_int_binary_func(
     lambda x, y: x + y, 2, 3)
 print(g.is_symmetric())
+
+e = TruthTable(['1101'])
+print(e.is_constant())
