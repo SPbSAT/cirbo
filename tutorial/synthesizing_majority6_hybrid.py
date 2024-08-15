c15af1cef8af73c518b0059f9bd4ecc052d2c90c
tbelova <yukikomodo@gmail.com>
2024-08-16 01:13:48 +0300

    feat tutorial: maj6 (#70)

--- tutorial/synthesizing_majority6_hybrid.py
+++ tutorial/synthesizing_majority6_hybrid.py
@@ -3,13 +3,13 @@ from boolean_circuit_tool.synthesis.circuit_search import CircuitFinderSat
 from boolean_circuit_tool.synthesis.generation.arithmetics import generate_sum_n_bits
 
 
-def geq3_py(x: bool, y: bool, z: bool):
+def geq3(x: bool, y: bool, z: bool):
     s = x + 2 * y + 4 * z
     return [DontCare] if s > 6 else [True] if s >= 3 else [False]
 
 ckt = generate_sum_n_bits(n=6)
-m = PyFunctionModel.from_positional(geq3_py)
-cfs = CircuitFinderSat(m, 2, basis='XAIG')
-geq3 = cfs.find_circuit()
-ckt.extend_circuit(geq3, name='geq3')
+pfm = PyFunctionModel.from_positional(geq3)
+cfs = CircuitFinderSat(pfm, 2, basis='XAIG')
+geq3_ckt = cfs.find_circuit()
+ckt.extend_circuit(geq3_ckt, name='geq3')
 ckt.view_graph()
