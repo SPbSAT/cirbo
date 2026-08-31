e6f20902449bdec678b307e5cf82801b5bc746ec
spefk <f.kurmazov.b@gmail.com>
2026-08-31 23:16:28 +0300

    snapshot

--- cirbo/minimization/metaheuristic/loaders.py
+++ cirbo/minimization/metaheuristic/loaders.py
@@ -1,15 +1,32 @@
 import abc
 import dataclasses
 import pathlib
+import typing as tp
 
 from cirbo.core import Circuit
 
 
 @dataclasses.dataclass
 class InstanceDescriptor:
-    path: pathlib.Path
+    """
+    Describes an instance of a circuit to be minimized.
+
+    """
+
+    instance: tp.Union[pathlib.Path, Circuit]
     depth: int
     size: int
+
+    @classmethod
+    def from_circuit(cls, circuit: Circuit) -> "InstanceDescriptor":
+        return InstanceDescriptor(
+            instance=circuit,
+            depth=circuit.get_depth(),
+            size=circuit.size,
+        )
+
+    def dominates(self, other: "InstanceDescriptor") -> bool:
+        return self.size >= other.size and self.depth >= other.depth and self != other
 
 
 class InstanceAccessor(metaclass=abc.ABCMeta):
@@ -22,7 +39,13 @@ class InstanceAccessor(metaclass=abc.ABCMeta):
         raise NotImplementedError()
 
 
-class InstanceFront: ...
+class InstanceFront(metaclass=abc.ABCMeta):
+    def __init__(self):
+        self.instances: tp.List[InstanceDescriptor] = []
+
+    @abc.abstractmethod
+    def rebuild(self, new_instance: InstanceDescriptor) -> "InstanceFront":
+        raise NotImplementedError
 
 
-class InstanceParetoFront: ...
+class InstanceParetoFront(InstanceFront): ...
