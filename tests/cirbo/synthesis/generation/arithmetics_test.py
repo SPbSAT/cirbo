faf3e8d4bd0e70b6ef73719140c2c856b88e1a39
HSPyroblast <levsovga@ya.ru>
2026-09-18 10:40:10 +0300

    Generation: add log-depth multiplication and AIG basis

--- tests/cirbo/synthesis/generation/arithmetics_test.py
+++ tests/cirbo/synthesis/generation/arithmetics_test.py
@@ -25,6 +25,7 @@ from cirbo.synthesis.generation.arithmetics import (
     add_smul_wallace,
     add_sqrt,
     add_square,
+    add_square_dadda,
     add_square_pow2_m1,
     add_sub_two_numbers,
     add_sub_two_numbers_log_depth,
@@ -56,6 +57,7 @@ from cirbo.synthesis.generation.arithmetics._utils import (
     binary_tt_to_type,
     PLACEHOLDER_STR,
 )
+from cirbo.synthesis.generation.exceptions import BadBasisError
 
 TEST_SIZE = 100
 random.seed(42)
@@ -436,7 +438,6 @@ def test_gen_mul_with_basis(type, basis, big_endian):
         assert mul_naive(input_labels_a, input_labels_b) == res
 
 
-@pytest.mark.parametrize("func", [add_square, add_square_pow2_m1])
 @pytest.mark.parametrize(
     "x",
     [
@@ -449,14 +450,20 @@ def test_gen_mul_with_basis(type, basis, big_endian):
     ],
 )
 @pytest.mark.parametrize("big_endian", [True, False])
-def test_square(func, x, big_endian):
+def test_square(x, big_endian):
     ckt = Circuit()
     input_labels = [f'x{i}' for i in range(x)]
     for i in range(x):
         ckt.add_gate(Gate(input_labels[i], INPUT))
 
-    res = func(ckt, input_labels, big_endian=big_endian)
+    res = add_square(
+        ckt,
+        input_labels,
+        basis=GenerationBasis.XAIG,
+        big_endian=big_endian,
+    )
     ckt.set_outputs(res)
+    assert_circuit_in_basis(ckt, GenerationBasis.XAIG)
 
     for test in range(TEST_SIZE):
         input_labels = [random.choice([0, 1]) for _ in range(x)]
@@ -468,7 +475,65 @@ def test_square(func, x, big_endian):
         assert square_naive(input_labels) == res
 
 
-@pytest.mark.parametrize("type", [SquareMode.DEFAULT, SquareMode.POW2_M1])
+@pytest.mark.parametrize("big_endian", [True, False])
+def test_square_unsupported_basis(big_endian):
+    ckt = Circuit()
+    input_labels = [f'x{i}' for i in range(2)]
+    for label in input_labels:
+        ckt.add_gate(Gate(label, INPUT))
+
+    with pytest.raises(BadBasisError):
+        add_square(
+            ckt,
+            input_labels,
+            basis=GenerationBasis.AIG,
+            big_endian=big_endian,
+        )
+
+
+@pytest.mark.parametrize("func", [add_square_pow2_m1, add_square_dadda])
+@pytest.mark.parametrize(
+    "x",
+    [
+        1,
+        2,
+        5,
+        7,
+        pytest.param(17, marks=pytest.mark.slow),
+    ],
+)
+@pytest.mark.parametrize("basis", [GenerationBasis.XAIG, "AIG"])
+@pytest.mark.parametrize("big_endian", [True, False])
+def test_square_with_basis(func, x, basis, big_endian):
+    ckt = Circuit()
+    input_labels = [f'x{i}' for i in range(x)]
+    for i in range(x):
+        ckt.add_gate(Gate(input_labels[i], INPUT))
+
+    res = func(ckt, input_labels, basis=basis, big_endian=big_endian)
+    ckt.set_outputs(res)
+    assert_circuit_in_basis(ckt, basis)
+
+    for test in range(TEST_SIZE):
+        input_labels = [random.choice([0, 1]) for _ in range(x)]
+        res = ckt.evaluate(input_labels)
+        if big_endian:
+            input_labels.reverse()
+        else:
+            res.reverse()
+        assert square_naive(input_labels) == res
+
+
+@pytest.mark.parametrize(
+    "type,basis",
+    [
+        (SquareMode.DEFAULT, GenerationBasis.XAIG),
+        (SquareMode.POW2_M1, GenerationBasis.XAIG),
+        (SquareMode.POW2_M1, "AIG"),
+        (SquareMode.DADDA, GenerationBasis.XAIG),
+        (SquareMode.DADDA, "AIG"),
+    ],
+)
 @pytest.mark.parametrize(
     "number_inputs",
     [
@@ -481,12 +546,14 @@ def test_square(func, x, big_endian):
     ],
 )
 @pytest.mark.parametrize("big_endian", [True, False])
-def test_gen_square(number_inputs, type, big_endian):
+def test_gen_square(number_inputs, type, basis, big_endian):
     ckt: Circuit = generate_square(
         number_inputs,
         type=type,
+        basis=basis,
         big_endian=big_endian,
     )
+    assert_circuit_in_basis(ckt, basis)
     for test in range(TEST_SIZE):
         input_labels = [random.choice([0, 1]) for _ in range(number_inputs)]
         res = ckt.evaluate(input_labels)
@@ -495,6 +562,17 @@ def test_gen_square(number_inputs, type, big_endian):
         else:
             res.reverse()
         assert square_naive(input_labels) == res
+
+
+@pytest.mark.parametrize("big_endian", [True, False])
+def test_gen_square_unsupported_basis(big_endian):
+    with pytest.raises(BadBasisError):
+        generate_square(
+            2,
+            type=SquareMode.DEFAULT,
+            basis=GenerationBasis.AIG,
+            big_endian=big_endian,
+        )
 
 
 @pytest.mark.parametrize(
